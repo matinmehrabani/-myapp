42294574047a720f6842e9ffc07c4d8162b9284c
Matin <matin.mehrabani1397@gmail.com>
2019-05-25 11:00:43 +0430

    first commit

---
+++ Mikepenz/app/src/main/java/com/example/mikepenz/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mikepenz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mikepenz.materialdrawer.DrawerBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new DrawerBuilder().withActivity(this).build();
+    }
+}
